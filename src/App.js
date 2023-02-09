64b44507b08bcc0b26a0176121356c79d7b06620
JellyKingdom <jyhan1993@gmail.com>
2023-02-09 13:38:09 +0900

    feat: styled-component flex

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import styled from 'styled-components';
-import './App.css';
+import styled from "styled-components";
+import "./App.css";
 
 //styled.뒤 에는 항상 html요소가 온다!
-//``안에는 css문법을 써준다! 
+//``안에는 css문법을 써준다!
 const StBox = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 1px solid ${(props) => {
-    return props.borderColor;
-    }};
-  background-color: ${props => props.backgroundColor};
-  margin: 20px;
+    width: 100px;
+    height: 100px;
+    border: 1px solid
+        ${(props) => {
+            return props.borderColor;
+        }};
+    margin: 20px;
+`;
+
+const StContainer = styled.div`
+    display: flex;
 `;
 
 function App() {
-  return (
-    <>
-      <StBox borderColor="red" backgroundColor="yellow">
-        빨간박스
-      </StBox>
-      <StBox borderColor="blue" backgroundColor="red">
-        파란박스
-      </StBox>
-      <StBox borderColor="green" backgroundColor="blue">
-        초록박스
-      </StBox>
-    </>
-  );
+    return (
+        <>
+            <StContainer>
+                <StBox borderColor="red">
+                    빨간박스
+                </StBox>
+                <StBox borderColor="blue">
+                    파란박스
+                </StBox>
+                <StBox borderColor="green">
+                    초록박스
+                </StBox>
+            </StContainer>
+        </>
+    );
 }
 
 //props : 부모 컴포넌트 -> 자식 컴포넌트
