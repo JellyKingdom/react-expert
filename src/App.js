31b9d84e2e19cb0103cfa9f07733e14bd3377d9a
JellyKingdom <jyhan1993@gmail.com>
2023-02-09 13:29:54 +0900

    feat: styled-component using props, arrow function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import './App.css';
 const StBox = styled.div`
   width: 100px;
   height: 100px;
-  border: 1px solid ${props =>props.borderColor};
+  border: 1px solid ${(props) => {
+    return props.borderColor;
+    }};
   margin: 20px;
 `;
 
