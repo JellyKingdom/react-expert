91fc0465d426e5638acf46ae3565336bfe01041c
JellyKingdom <jyhan1993@gmail.com>
2023-02-09 09:33:23 +0900

    feat: practice 2: styled p

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import './App.css';
 
+//styled.뒤 에는 항상 html요소가 온다!
+//``안에는 css문법을 써준다! 
 const StBox = styled.div`
   width: 100px;
   height: 100px;
@@ -8,12 +10,15 @@ const StBox = styled.div`
   margin: 20px;
 `;
 
+const StP = styled.p`
+  color: blue;
+`
 
 function App() {
   return (
     <>
       <StBox>
-        hello react-expert!
+        <StP>나는 p태그 입니다.</StP>
       </StBox>
     </>
   );
