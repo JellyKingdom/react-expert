96baa1a0dfad7a5f0434a63df372a0dca76d4d58
JellyKingdom <jyhan1993@gmail.com>
2023-02-09 09:30:47 +0900

    feat: practice 1 styled div

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import styled from 'styled-components';
 import './App.css';
+
+const StBox = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 1px solid red;
+  margin: 20px;
+`;
+
 
 function App() {
   return (
     <>
-      <div>
+      <StBox>
         hello react-expert!
-      </div>
+      </StBox>
     </>
   );
 }
