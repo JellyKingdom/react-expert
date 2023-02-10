88641341a3833e1dfbcd435ba2e8f29096dbb73f
JellyKingdom <jyhan1993@gmail.com>
2023-02-10 13:54:09 +0900

    feat: focus on password input using useEffect and useRef

--- src/App.js
+++ src/App.js
@@ -2,38 +2,25 @@ import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import "./App.css";
 
-const style = {
-    border: "1px solid black",
-    padding: "10px",
-    margin: "10px",
-};
-
 function App() {
-    const [count, setCount] = useState(0);
-    const countRef = useRef(0);
+  
+  const idRef = useRef('');
+  const pwRef = useRef('');
 
-    //렌더링이 계속 일어나기때문에 숫자가 계속 +1씩 늘어난다.
-    const plusStateCountButtonHandler = () => {
-      setCount(count + 1);
-    }
-
-
-    //렌더링을 일으키지않기때문에 화면속에서 변화는 없지만 콘솔로 값이 변하는건 볼 수 있다.
-    const plusRefCountButtonHandler = () => {
-      countRef.current++;
-      console.log(countRef.current);
-    }
+  // 화면이 랜더링 될때 어떤 작업을 하고 싶다! : useEffect!
+  useEffect(()=> {
+    // idRef.current.focus();
+    pwRef.current.focus();
+  },[])
 
     return (
         <>
-            <div style={style}>
-                state 영역입니다. {count} <br />
-                <button onClick={plusStateCountButtonHandler}>state 증가</button>
-            </div>
-            <div style={style}>
-                ref 영역입니다. {countRef.current} <br />
-                <button onClick={plusRefCountButtonHandler}>ref 증가</button>
-            </div>
+          <div>
+            아이디 : <input type="text" ref={idRef}/>
+          </div>
+          <div>
+            비밀번호 : <input type="password" ref={pwRef}/>
+          </div>
         </>
     );
 }
