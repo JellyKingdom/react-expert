63aa768ec49a4d32496c8c5d5f4757e1a2ca6ff9
JellyKingdom <jyhan1993@gmail.com>
2023-02-10 10:29:45 +0900

    feat: functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import styled from "styled-components";
 import "./App.css";
 
+//React Hooks - useState
+
+//가장 기본적인 hook
+//함수형 컴포넌트 내에서 가변적인 상태를 갖게 함
+//카운터, Todolist
 
 
 function App() {
+
+  const [number, setNumber] = useState(0);
     return (
         <>
-        <span>Default Style 테스트</span>
-        <h1>이건 h1 태그입니다.</h1>
-        <p>나는 p태그입니다.</p>
-      
-        {/*p태그는 크롬에서 기본 마진16px이 제공됨 
-        하지만 브라우저마다 default 스타일이 다르다. 그래서 default 스타일을 제거 해줄 필요가 있다.
-        */}
+        <span>UseState</span>
+        <div>Number State : {number}</div>
+        <button onClick={() => {
+          //setNumber(number + 1);
+          //아래에 함수형 컴포넌트로 표현
+          setNumber((currentState) => {
+            return currentState + 1; 
+          })
+        }}>누르면 up</button>
         </>
     );
 }
