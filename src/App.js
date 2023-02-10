628f91c057560ee39a9fc8349b89b4bbd59ddd63
JellyKingdom <jyhan1993@gmail.com>
2023-02-10 09:43:39 +0900

    feat: add reset.css and import to index.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import "./App.css";
 function App() {
     return (
         <>
+        <span>Default Style 테스트</span>
+        <h1>이건 h1 태그입니다.</h1>
         <p>나는 p태그입니다.</p>
+      
         {/*p태그는 크롬에서 기본 마진16px이 제공됨 
         하지만 브라우저마다 default 스타일이 다르다. 그래서 default 스타일을 제거 해줄 필요가 있다.
         */}
