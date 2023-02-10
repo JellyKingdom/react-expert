7d77cb233ac87cf71edfa45b4d7cc3f3d57add03
JellyKingdom <jyhan1993@gmail.com>
2023-02-10 13:30:26 +0900

    feat: useRef1

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import "./App.css";
 
 function App() {
+  //ref : reference
+  const ref = useRef('초기값');
+  // key는 current, value는 여기에 설정해준 '초기값'
+  console.log('ref',ref);
 
-  const [value, setValue] = useState('');
-  //useEffct
-  //렌더링 될 때, 특정한 작업을 수행해야할 때 설정하는 훅
+  ref.current = '변경값';
+  console.log('ref2',ref)
 
-  // 컴포넌트가 화면에 보여졌을 때
-  // 컴포넌트가 화면에서 사라졌을 때(return)
+  //이렇게 설정된 ref값은 컴포넌트가 계속해서 렌더링 되어도 unmount전까지 값을 유지한다.
 
-  //1.input에 값을 입력
-  //2. value, 즉 state가 변경
-  //3. state가 바뀌었기때문에 App 컴포넌트가 리렌더링
-  //4. 리렌더링 -> useEffect()
-  
-  //Dependancy Array
-  //이 배열에 값을 넣으면, 그 값이 바뀔 때만 useEffect를 실행한다.
-
-  //clean up
-  useEffect(() => {
-    console.log(`hello, useEffect : ${value}`);
-
-    return () => {
-      // 컴포넌트가 사라질때 동작
-      console.log('나 사라져요 ..ㅠ');
-    }
-  }, [value]);
-
+  // state는 리렌더링이 꼭 필요한 값을 다룰 때 쓰면 된다.
+  // ref는 리렌더링을 발생시키지 않는 값을 저장할 때 사용한다.
     return (
         <>
-        <span>UseEffect</span>
-        <input
-        type="text"
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-        />
+          <div>useRef</div>
         </>
     );
 }
