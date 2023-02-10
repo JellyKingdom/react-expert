f8c790cd9aaada533dc40639b821416dba819c42
JellyKingdom <jyhan1993@gmail.com>
2023-02-10 16:24:58 +0900

    feat: make counter with useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
-import GrandFather from "./components/GrandFather";
+import { useState } from "react";
+
 
 function App() {
+    console.log('App컴포넌트가 렌더링 되었습니다.');
+
+    const [count, setCount] = useState(0);
+
+    // 1 증가
+    const onPlusButtonClickHandler = () => {
+        setCount(count + 1);
+    } 
+
+    // 1 감소
+    const onMinusButtonClickHandler = () => {
+        setCount(count - 1);
+    }
     return <>
-    <GrandFather/>
+    <h3>카운트 예제입니다!</h3>
+    <p> 현재 카운트 : {count}</p>
+    <button onClick={onPlusButtonClickHandler}>+</button>
+    <button onClick={onMinusButtonClickHandler}>-</button>
     </>;
 }
 
 export default App;
+
+
