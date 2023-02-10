80e04c90ab550f1575829a8f4c726f541f3a179e
JellyKingdom <jyhan1993@gmail.com>
2023-02-10 20:54:29 +0900

    useMemo practice: Object component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import "./App.css";
-import HeavyComponent from "./components/HeavyComponent";
+
+import ObjectComponent from "./components/ObjectComponent";
 
 // heavy work -> 엄청 무거운 작업!
 function App() {
     return <>
-        <nav style={{
-            backgroundColor: 'yello',
-            marginBottom: '30px',
-        }}>네비게이션 바</nav>
-        <HeavyComponent/>
-        <footer style={{
-            backgroundColor: 'green',
-            marginBottom: '30px',
-        }}>푸터 영역입니당!</footer>
+        <ObjectComponent/>
 
     </>;
 }
