e567515a8e0e46d60c7f7e180b4866055ad73fa4
JellyKingdom <jyhan1993@gmail.com>
2023-02-10 14:01:38 +0900

    practice useRef2

--- src/App.js
+++ src/App.js
@@ -7,16 +7,20 @@ function App() {
   const idRef = useRef('');
   const pwRef = useRef('');
 
+  const [id,setId] = useState('');
+
   // 화면이 랜더링 될때 어떤 작업을 하고 싶다! : useEffect!
   useEffect(()=> {
-    // idRef.current.focus();
-    pwRef.current.focus();
+    
   },[])
+
+  if(id.length === 10){
+    pwRef.current.focus();}
 
     return (
         <>
           <div>
-            아이디 : <input type="text" ref={idRef}/>
+            아이디 : <input type="text" value={id} ref={idRef} onChange={(e) => {setId(e.target.value)}}/>
           </div>
           <div>
             비밀번호 : <input type="password" ref={pwRef}/>
