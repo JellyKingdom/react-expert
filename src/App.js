c05e34ae5c6e91382a0b5acc6b704b20e07541d7
JellyKingdom <jyhan1993@gmail.com>
2023-02-10 10:40:13 +0900

    feat: functional component2

--- src/App.js
+++ src/App.js
@@ -17,11 +17,22 @@ function App() {
         <span>UseState</span>
         <div>Number State : {number}</div>
         <button onClick={() => {
+          //아래의 3개의 setNumber(number + 1)은 +1만 된다.
+          //배치성으로 처리가 된다.
+          //배치업데이트!
           //setNumber(number + 1);
+          //setNumber(number + 1);
+          //setNumber(number + 1);
+
+          //렌더링이 잦다 -> 성능에 이슈가 있다.
+          //불필요한 렌더링을 피하기위해 요청사항을 모아서 한번에 처리
+          
           //아래에 함수형 컴포넌트로 표현
-          setNumber((currentState) => {
-            return currentState + 1; 
-          })
+          // 3개의 함수형 컴포넌트는 +3이 된다.
+          //명령을 모아서 순차적으로 하나씩 실행시킨다!
+          setNumber((currentState) => currentState + 1);
+          setNumber((currentState) => currentState + 1);
+          setNumber((currentState) => currentState + 1);
         }}>누르면 up</button>
         </>
     );
