f3f20bc3b41a9e6322f421d84255f7ac12f143a8
JellyKingdom <jyhan1993@gmail.com>
2023-02-11 10:19:07 +0900

    feat: folder convention and config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import "./App.css";
 
-import ObjectComponent from "./components/ObjectComponent";
-
-// heavy work -> 엄청 무거운 작업!
 function App() {
     return <>
-        <ObjectComponent/>
-
+        Redux!
     </>;
 }
 
