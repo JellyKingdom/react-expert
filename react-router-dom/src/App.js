86e14c2b12afa94e7dbd543dc93b3e77a99ac1e3
JellyKingdom <jyhan1993@gmail.com>
2023-02-12 14:59:26 +0900

    feat: Router setting

---
+++ react-router-dom/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Router from './shared/Router';
+
+function App() {
+  return (
+  <>
+  <Router />
+
+  </>
+  );
+}
+
+export default App;
