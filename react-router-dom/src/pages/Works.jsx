17a009960c8abc411362a59d5c1b1b416b537513
JellyKingdom <jyhan1993@gmail.com>
2023-02-12 15:14:37 +0900

    feat: Link

--- react-router-dom/src/pages/Works.jsx
+++ react-router-dom/src/pages/Works.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 function Works() {
-    const nav = useNavigate();
+    // const nav = useNavigate();
     const location = useLocation();
 
     console.log('location :',location);
 
     return <div>Works<br/>
     <button onClick={() => {
-        nav('/');
-    }}>Home으로 이동</button>
+        // nav('/');
+    }}>Home으로 이동</button><br/>
+    <Link to='/contact'>Contact 페이지로 이동하기</Link>
     </div>;
 }
 
