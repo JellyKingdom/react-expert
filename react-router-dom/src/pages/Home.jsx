eaf24c75ba13644302ae72f46d3b64b6f3d6391a
JellyKingdom <jyhan1993@gmail.com>
2023-02-12 15:10:29 +0900

    feat: useNavigate()

--- react-router-dom/src/pages/Home.jsx
+++ react-router-dom/src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-function home() {
-    return <div>home</div>;
+
+function Home() {
+    const nav = useNavigate();
+
+    return <div>Home <br/>
+    <button onClick={() => {
+        nav("/works");
+    }}>works로 이동</button>
+    </div>;
 }
 
-export default home;
+export default Home;
--- react-router-dom/src/pages/Works.jsx
+++ react-router-dom/src/pages/Works.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Works() {
-    return <div>Works</div>;
+    const nav = useNavigate();
+    return <div>Works<br/>
+    <button onClick={() => {
+        nav('/');
+    }}>Home으로 이동</button>
+    </div>;
 }
 
 export default Works;
