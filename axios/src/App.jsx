041a8a6f44fcb8a94b63627ab33fe9f660a962ec
JellyKingdom <jyhan1993@gmail.com>
2023-02-17 21:49:17 +0900

    practice axios 1

--- axios/src/App.jsx
+++ axios/src/App.jsx
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useEffect,useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState(null);
+
+  const fetchTodos = async () => {
+    const {data} = await axios.get('http://localhost:4000/todos');
+    console.log('data',data);
+    setTodos(data);
+  }
+  useEffect(()=>{
+    fetchTodos();
+    // db로부터 값을 가져올 것이다.
+
+  }, []);
   return (
     <>
-      axios
+
+    {
+      todos?.map(item => {
+        return (
+          <div key={item.id}>
+            {item.id} : {item.title}
+          </div>
+        )
+      })
+    }
+
     </>
   );
 }
