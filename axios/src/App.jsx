0aba9b8646bbb37197c54c94672b357ac1676a2f
JellyKingdom <jyhan1993@gmail.com>
2023-02-18 09:51:33 +0900

    feat: update Todo with axios

--- axios/src/App.jsx
+++ axios/src/App.jsx
@@ -7,7 +7,10 @@ function App() {
   const [inputValue, setInputValue] = useState({
     title :'',
   })
+  const [targetId, setTargetId] = useState('');
+  const [contents, setContents] = useState('');
 
+  //조회 함수
   const fetchTodos = async () => {
     const {data} = await axios.get('http://localhost:4000/todos');
     setTodos(data);
@@ -24,8 +27,24 @@ function App() {
     axios.delete(`http://localhost:4000/todos/${id}`);
     setTodos(todos.filter((item) =>{
       return item.id !== id;
+    }));
+  }
+
+  //수정 함수
+  const onUpdateButtonClickHandler = async() => {
+    axios.patch(`http://localhost:4000/todos/${targetId}`,{
+      title: contents
+    });
+
+    setTodos(todos.map(item => {
+      if(item.id == targetId){
+        return {...item, title:contents}
+      }else{
+        return item;
+      }
     }))
   }
+
   useEffect(()=>{
     fetchTodos();
     // db로부터 값을 가져올 것이다.
@@ -33,6 +52,24 @@ function App() {
   }, []);
   return (
     <>
+    <div>
+      {/*수정 영역 */}
+      <input type="text" placeholder='수정할 아이디'
+      value={targetId}
+      onChange={(e) => {
+        setTargetId(e.target.value);
+      }}
+      />
+      <input type="text" placeholder='수정할 내용'
+      value={contents}
+      onChange={(e)=>{
+        setContents(e.target.value);
+      }}
+      />
+      <button onClick={onUpdateButtonClickHandler}>수정</button>
+    </div>
+    <br/>
+    <br/>
     <div>
       {/*input영역 */}
       <form onSubmit={(e) =>{
