f706aee143ef511ea06de83b08c2604bf3d0008c
JellyKingdom <jyhan1993@gmail.com>
2023-02-18 10:31:29 +0900

    feat: axios interceptors

--- axios/src/axios/api.js
+++ axios/src/axios/api.js
@@ -6,4 +6,32 @@ const instance = axios.create({
     baseURL: 'http://localhost:4000',
 });
 
+instance.interceptors.request.use(
+    //요청을 보내기 전 수행, request header나 이런부분을 바꿀 수 있음
+    function(config){
+        console.log('인터셉트 요청 성공!')
+        return config;
+    },
+
+    //오류 요청을 보내기 전 수행
+    function(error){
+        console.log('인터셉트 요청 오류!')
+        return Promise.reject(error);
+    }
+);
+
+instance.interceptors.response.use(
+    //서버로부터 정상 응답을 받은 경우
+    function(response){
+        console.log('인터셉트 정상 응답 수신');
+        return response;
+    },
+
+    //서버로부터 오류 응답을 받은 경우
+    function(error){
+        console.log('인터셉트 오류 응답 수신!')
+        return Promise.reject(error);
+    }
+);
+
 export default instance;
